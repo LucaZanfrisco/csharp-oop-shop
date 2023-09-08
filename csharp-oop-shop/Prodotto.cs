2bc23259538759d19e4aa4147069ccd6acf114bf
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-08 15:59:38 +0200

    Classe Prodotto con getters e setters, main che stampa il prodotto

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -9,25 +9,90 @@ namespace csharp_oop_shop
 {
     public class Prodotto
     {
+        // ATTRIBUTI
         private int codice;
         private string nome;
         private string descrizione;
         private float prezzo;
         private int iva;
 
+        //COSTRUTTORI
         public Prodotto(string nome, string descrizione, float prezzo, int iva)
         {
             SetCodice();
-            this.nome = nome;
-            this.descrizione = descrizione;
-            this.prezzo = prezzo;
-            this.iva = iva;
+            SetNome(nome);
+            SetDescrizione(descrizione);
+            SetPrezzo(prezzo);
+            SetIva(iva);
         }
 
-        public void SetCodice()
+        // SETTERS
+        private void SetCodice()
         {
             Random random = new Random();
             this.codice = random.Next(1, 99999999);
+        }
+
+        public void SetNome(string nome) 
+        {
+            this.nome = nome;
+        }
+
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+
+        public void SetPrezzo(float prezzo)
+        {
+            if (prezzo > 0)
+                this.prezzo = prezzo;
+            else
+                this.prezzo = 0;
+        }
+
+        public void SetIva(int iva)
+        {
+            if(iva > 0)
+                this.iva = iva;
+            else
+                this.iva = 0;
+        }
+
+        // GETTERS
+        public int GetCodice()
+        {
+            return this.codice;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public float GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public int GetIva() 
+        {
+            return this.iva;
+        }
+
+        public float GetPrezzoIva()
+        {
+            return MathF.Round(this.prezzo *(1 + ((float)iva / 100)) ,2);
+        }
+
+        public string GetNomeEsteso()
+        {
+            return $"{codice} {nome}";
         }
 
     }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -2,4 +2,17 @@
 
 using csharp_oop_shop;
 
-Console.WriteLine("Hello World!");
+Prodotto prodotto1 = new Prodotto("Margherita","Magnifico bouquet di margherite bianche", 22.0f, 24);
+
+string nome = prodotto1.GetNome();
+Console.WriteLine($"Nome: {nome}");
+
+string descrizione = prodotto1.GetDescrizione();
+Console.WriteLine($"Descrizione: {descrizione}");
+
+float prezzo = prodotto1.GetPrezzo();
+Console.WriteLine($"Prezzo pre-iva: {prezzo} euro");
+
+float prezzoIva = prodotto1.GetPrezzoIva(); 
+Console.WriteLine($"Prezzo {prezzoIva} euro");
+
