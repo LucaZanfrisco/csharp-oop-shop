efe1f0b384b536165cd49394662374efb46d62cc
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-08 15:28:00 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+        private int iva;
+
+        public Prodotto(string nome, string descrizione, float prezzo, int iva)
+        {
+            SetCodice();
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        public void SetCodice()
+        {
+            Random random = new Random();
+            this.codice = random.Next(1, 99999999);
+        }
+
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using csharp_oop_shop;
+
+Console.WriteLine("Hello World!");
